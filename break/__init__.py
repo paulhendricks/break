a5f78f056839dbc4fa359040fde6cb462d85ad85
Paul Hendricks <paul.hendricks.2013@owu.edu>
2016-06-14 12:09:51 -0400

    some templating

---
+++ break/__init__.py
@@ -0,0 +1,7 @@
+# break/__init__.py
+
+__version__ = '0.1.0'
+
+from .blah import *
+
+__all__ += blah.__all__
---
+++ break/blah.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""blah
+
+"""
+
+__all__ = ['']
+
+
+def hello():
+    print("Hello world!")
